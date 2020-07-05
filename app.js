c22a2f8fbc441cb106d27cbd7cc020a4fe5a5135
Halit <60817499+halitos@users.noreply.github.com>
2020-07-05 15:06:26 +0100

    working fine

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let descriptionDisplay = document.querySelector(".temp-description");
+  let tempDisplay = document.querySelector(".temperature-degree");
+  let location = document.querySelector(".location-timezone"); //data.name? or data.system.timezone
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -15,8 +18,15 @@ window.addEventListener("load", () => {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          const weather = Math.round(data.main.temp - 273.15);
-          console.log(weather);
+          const weatherTemp = Math.round(data.main.temp - 273.15);
+          const weatherDescription = data.weather[0].description;
+          tempDisplay.innerText = weatherTemp;
+          descriptionDisplay.innerText = weatherDescription;
+          location.innerText = data.name;
+          const weatherIcon = data.weather[0].icon;
+          const iconImg = document.querySelector("img");
+          const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+          iconImg.setAttribute("src", iconUrl);
         });
     });
   }
